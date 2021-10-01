6205c1d2e98745e7ab2ced67db0dcec82205783a
Yusuf Demir <yusufdemir286634@gmail.com>
2021-10-02 01:11:25 +0300

    fix in cond

--- lib/decent_app.ex
+++ lib/decent_app.ex
@@ -49,45 +49,41 @@ defmodule DecentApp do
           if is_error do
             {nil, nil, true}
           else
-            new_balance = %{bal | coins: bal.coins - 1}
-
             res =
               cond do
-                command === "NOTHING" ->
+                command == "NOTHING" ->
+                  res
+
+                command == "DUP" ->
+                  res ++ [List.last(res)]
+
+                command == "POP" ->
+                  {_, res} = List.pop_at(res, length(res) - 1)
+                  res
+
+                command == "+" ->
+                  [first, second | rest] = Enum.reverse(res)
+                  Enum.reverse(rest) ++ [first + second]
+
+                command == "-" ->
+                  [first, second | rest] = Enum.reverse(res)
+                  Enum.reverse(rest) ++ [first - second]
+
+                command == "*" ->
+                  [first, second, third | rest] = Enum.reverse(res)
+                  Enum.reverse(rest) ++ [first * second * third]
+
+                is_integer(command) ->
+                  res ++ [command]
+
+                command == "COINS" ->
                   res
 
                 true ->
-                  cond do
-                    command == "DUP" ->
-                      res ++ [List.last(res)]
-
-                    true ->
-                      if command == "POP" do
-                        {_, res} = List.pop_at(res, length(res) - 1)
-                        res
-                      else
-                        cond do
-                          command == "+" ->
-                            [first, second | rest] = Enum.reverse(res)
-                            Enum.reverse(rest) ++ [first + second]
-
-                          command == "-" ->
-                            [first, second | rest] = Enum.reverse(res)
-                            Enum.reverse(rest) ++ [first - second]
-
-                          command == "*" ->
-                            [first, second, third | rest] = Enum.reverse(res)
-                            Enum.reverse(rest) ++ [first * second * third]
-
-                          is_integer(command) ->
-                            res ++ [command]
-
-                          command == "COINS" ->
-                            res
-                        end
-                      end
-                  end
+                  res
               end
+
+            new_balance = %{bal | coins: bal.coins - 1}
 
             new_balance =
               if command == "COINS" do
